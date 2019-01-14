2fead4dbe304eb7d2f849ac56da8d8329f62b34f
joannenea <cpccbaby@gmail.com>
2019-01-15 07:21:13 +0800

    Update pcap_offline.c

--- pcap_offline.c
+++ pcap_offline.c
@@ -1,95 +1,84 @@
-#include <pcap/pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <pcap.h>
 #include <time.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netinet/ip.h>
-#include <netinet/if_ether.h>
 #include <netinet/ether.h>
+#include <netinet/ip.h>
 #include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netpacket/packet.h>
+#include <arpa/inet.h>
 
-int main(int argc, char *argv[])
+
+int main(int argc, char** argv)
 {
+	char errbuf[512] = {'\0'};
+	struct pcap_pkthdr *header;
+	const struct ip *ip;
+	const struct tcphdr *tcp;
+	const u_char *packet;
+	u_int size_ip = sizeof(struct ip);
+	u_int size_ether = sizeof(struct ether_header);
 	int i;
-	char filter[30];
-	char errbuff[PCAP_ERRBUF_SIZE], tmbuf[64], buf[64];
-	const char filename[] = "amqps.pcapng";
-	int packet_num = 0;
+	int pktcnt = 0;
+	char filter[50];
 	strcpy(filter, "");
-	if(argc >= 2)
-	{
-		strcpy(filter, argv[1]);
-		for(i=2; i<argc; i++)
-		{
+	
+	if(argc >= 3){
+		i = 3;
+		strcpy(filter, argv[2]);
+		while(i<argc){
 			strcat(filter, " ");
 			strcat(filter, argv[i]);
+			i++;
 		}
+		//printf("%s\n",filter);
 	}
-	//open file offline
-	pcap_t *handle = pcap_open_offline(filename, errbuff);
-	if(!handle)
-	{
-		printf("pcap_open_offline() : %s\n",errbuff);
+
+	// open pcap file
+	pcap_t *handle = pcap_open_offline(argv[1],errbuf);
+	if(!handle){
+		printf("fail to open %s\n", argv[1]);
 		exit(1);
 	}
-	printf("Open file : %s\n",filename);
+	printf("File name : %s\n\n", argv[1]);
 
-	struct bpf_program filter_code;
+
+
 	//compile filter
-	if(pcap_compile(handle, &filter_code, filter, 1, PCAP_NETMASK_UNKNOWN) == -1)
+    struct bpf_program fcode;
+    if(-1 == pcap_compile(handle, &fcode, filter, 1, PCAP_NETMASK_UNKNOWN)) {
+        fprintf(stderr, "pcap_compile(): %s\n", pcap_geterr(handle));
+        pcap_close(handle);
+        exit(1);
+    }//end if
+
+
+
+	while(pcap_next_ex(handle, &header, &packet) >= 0)
 	{
-		printf("pcap_compile : %s\n", pcap_geterr(handle));
-		pcap_close(handle);
-		exit(1);
-	}
-	if(strlen(filter)!=0)
-		printf("Filter : %s\n", filter);
-	//packet reading
-	while(1)
-	{
-		struct pcap_pkthdr *header = NULL;
-		const u_char *content = NULL;
-		int ret = pcap_next_ex(handle, &header, &content);
-		if(ret == 1)
-		{
-			if(pcap_offline_filter(&filter_code, header, content) != 0)
-			{
-				//show packet number
-				printf("----------------\n");
-				printf("Packet # %d\n", ++packet_num);	
-				
-				//show the size in bytes of the packet
-				printf("Packet size : %d bytes\n", header->len);
+		if(pcap_offline_filter(&fcode, header, packet) != 0) {  
+            
+			char tmbuf[64];
+			time_t nowtime = header->ts.tv_sec;
+			struct tm *nowtm = localtime(&nowtime);
+			strftime(tmbuf, sizeof(tmbuf), "%Y-%m-%d %H:%M:%S", nowtm);
 
-				//show Epoch time
-				time_t nowtime = header->ts.tv_sec;
-				struct tm *nowtm;
-				nowtm = localtime(&nowtime);
-				strftime(tmbuf, sizeof(tmbuf), "%Y-%m-%d %H:%M:%S", nowtm);
-				printf("Epoch Time : %s.%06ld\n", tmbuf, header->ts.tv_sec);
+			ip = (struct ip*)(packet + size_ether);
+			tcp = (struct tcphdr*)(packet + size_ether + size_ip);
 
-	//			printf("Epoch Time : %s", asctime(gmtime(((time_t)&header->ts.tv_sec))));		
-				
-				//show port and address
-				struct ip *ip;
-				struct tcphdr *tcp;
-				ip = (struct ip*)(content+sizeof(struct ether_header));
-				tcp = (struct tcphdr*)(content+sizeof(struct ether_header)+sizeof(struct ip));
+			printf("Packet # %d\n", pktcnt++);
+			printf("Packet size :\t%d bytes\n", header->len);
+			printf("Epoch time :\t%s.%06ld\n", tmbuf, header->ts.tv_usec);
+			printf("src port :\t%d\n", tcp->source);
+			printf("dst port :\t%d\n", tcp->dest);
+			printf("src addr :\t%s\n", inet_ntoa(ip->ip_src));
+			printf("dst addr :\t%s\n", inet_ntoa(ip->ip_dst));
+			printf("=====\n");
 
-				printf("Source port : %d\n", tcp->source);//source port
-				printf("Destination port : %d\n", tcp->dest);//dest port
-
-				printf("Source address : %s\n", inet_ntoa(ip->ip_src));//source addr
-				printf("Destination address : %s\n",inet_ntoa(ip->ip_dst));//dest addr
-			}
-		}
-		else if(ret == -2)
-		{
-			printf("No more packet from file.\n");
-			break;
-		}
+        }
 	}
 	pcap_close(handle);
 	return 0;
